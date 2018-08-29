7f30cc7b222adfa4b96021e565207b31a99a48ce
cristianmarint <31068007+cristianmarint@users.noreply.github.com>
2018-08-28 21:00:33 -0500

    punto ocho

---
+++ Semestre IV/Programacion Orientada a Objetos/1 Corte/Taller 3/src/puntos/ocho.java
@@ -0,0 +1,24 @@
+package puntos;
+
+import java.util.Scanner;
+
+public class ocho {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner (System.in);
+		
+		float area;
+		double v,h=443.1792;
+		
+		System.out.println("Altura: pies 1,454 (443.1792 metros)");
+
+		v=(2*(9.8*h)); 
+	
+		System.out.println("El area del cuadrado es: "+Math.sqrt(v)+" metros por segundo");
+		
+	}
+
+
+}
