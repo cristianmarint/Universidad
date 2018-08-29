14846352307f0de1558eb08ee55cff4abcb22fc2
cristianmarint <31068007+cristianmarint@users.noreply.github.com>
2018-08-29 14:25:52 -0500

    punto 12

---
+++ Semestre IV/Programacion Orientada a Objetos/1 Corte/Taller 3/src/puntos/doce.java
@@ -0,0 +1,35 @@
+package puntos;
+
+import java.util.Scanner;
+
+public class doce {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner (System.in);
+		
+		float cat,x,a,b,c,d;
+		
+		System.out.println("Ingresa los valores X,A,B,C a continuacion:");
+		
+		
+		x=sc.nextFloat();
+		a=sc.nextFloat();
+		b=sc.nextFloat();
+		c=sc.nextFloat();
+		d=sc.nextFloat();
+
+		if(x<0) {
+			cat = (a+b-c+d);			
+		}else if(x==0) {
+				cat = ( (a+b)/(c+d) );			
+			}else {		
+					cat = ( (a+b)*(c+d) );
+				}
+		
+		System.out.println("El valor de CAT es: "+cat);
+		
+	}
+
+}
