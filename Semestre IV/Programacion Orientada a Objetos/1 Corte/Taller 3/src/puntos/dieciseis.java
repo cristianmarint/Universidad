84a1cce08532df43f11ed53b9bc97a04343b714c
cristianmarint <31068007+cristianmarint@users.noreply.github.com>
2018-08-29 15:13:55 -0500

    punto 16

---
+++ Semestre IV/Programacion Orientada a Objetos/1 Corte/Taller 3/src/puntos/dieciseis.java
@@ -0,0 +1,29 @@
+package puntos;
+
+import java.util.Scanner;
+
+public class dieciseis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner (System.in);
+		
+		float monto,deuda;
+		
+		System.out.println("Ingresa el monto a continuacion:");
+		monto=sc.nextFloat();
+		System.out.println("Ingresa la deuda a continuacion:");
+		deuda=sc.nextFloat();
+		
+		if(monto==deuda) {
+			System.out.println("El cliente esta ha pagado toda sus deudas");
+		}else if(monto>deuda) {
+			System.out.println("Creadito a favor por: "+(monto-deuda));
+		}else {
+			System.out.println("La nueva deuda es de: "+ ((deuda-monto)+(monto*0.03)) );
+		}
+		
+	}
+
+}
