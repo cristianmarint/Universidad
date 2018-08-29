d622eacc76a9b701ca95bd5b16ba163ae9533cb7
cristianmarint <31068007+cristianmarint@users.noreply.github.com>
2018-08-28 20:20:36 -0500

    punto 5

---
+++ Semestre IV/Programacion Orientada a Objetos/1 Corte/Taller 3/src/puntos/cinco.java
@@ -0,0 +1,27 @@
+package puntos;
+
+import java.util.Scanner;
+
+public class cinco {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner (System.in);
+		
+		float n,x;
+		
+		System.out.println("Ingresa los valores dos continuacion:");
+		
+		
+		n=sc.nextFloat();
+		x=sc.nextFloat();
+	
+		System.out.println("El valor de la suma es de: "+(n+x));
+		System.out.println("El valor de la multiplicacion es de: "+(n*x));
+		System.out.println("El valor de la division es de: "+(n/x));
+		
+	}
+
+
+}
