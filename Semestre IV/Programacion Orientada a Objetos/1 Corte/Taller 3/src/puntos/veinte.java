708d297a40b05253495d26f8bbd1245a6d6beb19
cristianmarint <31068007+cristianmarint@users.noreply.github.com>
2018-08-29 21:27:29 -0500

    punto 20

---
+++ Semestre IV/Programacion Orientada a Objetos/1 Corte/Taller 3/src/puntos/veinte.java
@@ -0,0 +1,19 @@
+package puntos;
+
+import java.util.Scanner;
+
+public class veinte {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Tabla de Multiplicar del 5 :o");
+		
+		for(int i=1;i<=10;i++) {
+			System.out.print(" , 5*"+i+"="+(i*5));
+		}
+	}
+
+}
