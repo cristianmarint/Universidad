ef550d71acde5940bfa350be3d9a5ff3f32ab531
cristianmarint <31068007+cristianmarint@users.noreply.github.com>
2018-08-30 14:59:21 -0500

    punto 23

---
+++ Semestre IV/Programacion Orientada a Objetos/1 Corte/Taller 3/src/puntos/veintitres.java
@@ -0,0 +1,18 @@
+package puntos;
+
+import java.util.Scanner;
+
+public class veintitres {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Sucesion:");
+		
+		for(int i=1;i<=500;i+=4) {
+			System.out.println(" , "+i);
+		}
+	}
+
+}
