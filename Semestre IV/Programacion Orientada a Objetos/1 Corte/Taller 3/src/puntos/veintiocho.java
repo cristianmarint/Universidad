ccf1ad1a67b325480d6978129a7572c4804897b3
cristianmarint <31068007+cristianmarint@users.noreply.github.com>
2018-08-30 18:22:41 -0500

    Punto 28
    Correccion en lectura

--- Semestre IV/Programacion Orientada a Objetos/1 Corte/Taller 3/src/puntos/veintiocho.java
+++ Semestre IV/Programacion Orientada a Objetos/1 Corte/Taller 3/src/puntos/veintiocho.java
@@ -7,24 +7,15 @@ public class veintiocho {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int [ ] [ ] matriz = new int [ 4 ] [ 4 ] ;
+		int [ ] [ ] matriz = { {1,2,2,1},{3,4,4,3},{5,6,6,4},{7,8,8,7} } ;
 		int [ ] vector = new int [ 4 ] ;
 		int sumaFilas=0,sumaColumnas=0;
 		int i=0,j=0,k=0,l=0;
 		
-		Scanner sc=new Scanner(System.in);
-		
-		System.out.println("Ingrese 16 numeros:");
+		System.out.println("Contenido de la matriz:");
 		for(i=0;i<4;i++) {
 			for(j=0;j<4;j++) {
-				matriz[i][j]=sc.nextInt();
-			}
-		}
-		
-		System.out.println("Numeros ingresados:");
-		for(i=0;i<4;i++) {
-			for(j=0;j<4;j++) {
-				System.out.print(matriz[i][j]);
+				System.out.print(" "+matriz[i][j]);
 			}
 			System.out.println("  ");
 		}
@@ -40,9 +31,6 @@ public class veintiocho {
 		for(i=0;i<4;i++) {
 			System.out.println("Suma de las columnas "+(1+i)+" - "+vector[i]);
 		}
-		
-		//System.out.println("Suma de valores ingresados en las filas: "+sumaFilas);
-		//System.out.println("Suma de valores ingresados en las columnas: "+sumaColumnas);
 		
 	}
 
