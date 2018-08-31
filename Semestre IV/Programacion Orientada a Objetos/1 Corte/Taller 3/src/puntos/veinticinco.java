0e7543dfcd6a007c56b0a8a45ebc98ac6903838a
cristianmarint <31068007+cristianmarint@users.noreply.github.com>
2018-08-30 23:07:41 -0500

    punto 25
    no terminado

--- Semestre IV/Programacion Orientada a Objetos/1 Corte/Taller 3/src/puntos/veinticinco.java
+++ Semestre IV/Programacion Orientada a Objetos/1 Corte/Taller 3/src/puntos/veinticinco.java
@@ -5,10 +5,21 @@ import java.util.Scanner;
 public class veinticinco {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
-		
+		// TODO Auto-generated method stub		
 	
+		int prestamo=200,anos=5;
+		double totalAPagar = (prestamo+(prestamo*0.06));
+		double cuota = (totalAPagar/anos);
+		double deuda= (totalAPagar);
+		
+		System.out.println("Miguel debera pagar cada año así:");
+		System.out.println(totalAPagar+" | "+cuota+" | "+deuda);
+		
+		for(int i=1;i<=5;i++) {
+			System.out.println("Año:"+i+" Cuota:"+cuota+" Deuda:"+(deuda-(cuota*i))+" Pagado:"+(totalAPagar-(cuota*i)));
+		}
+		
+		
 		
 	}
 
