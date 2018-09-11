f01af4549679145d261345d5e9467c1d5c12b3c5
cristianmarint <31068007+cristianmarint@users.noreply.github.com>
2018-09-11 17:06:43 -0500

    punto uno
    terminado :smile:

--- Semestre IV/Programacion Orientada a Objetos/1 Corte/Taller 5/punto1/src/punto1/Main.java
+++ Semestre IV/Programacion Orientada a Objetos/1 Corte/Taller 5/punto1/src/punto1/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		empleado emp = new empleado();
 		
+		emp.showDatos();
+		emp.impuestos();
+
 	}
 
 }
--- Semestre IV/Programacion Orientada a Objetos/1 Corte/Taller 5/punto1/src/punto1/empleado.java
+++ Semestre IV/Programacion Orientada a Objetos/1 Corte/Taller 5/punto1/src/punto1/empleado.java
@@ -1,5 +1,28 @@
 package punto1;
-
+import java.util.Scanner;
 public class empleado {
+	
+	String nombre;
+	float salario;
+	
+	public empleado() {
+		Scanner  sc = new Scanner(System.in);
+		System.out.println("Ingrese el nombre del empleado: ");
+		this.nombre = sc.nextLine();
+		System.out.println("Ingrese el salario del empleado: ");
+		this.salario = sc.nextFloat();
+	}
+	
+	public void showDatos() {
+		System.out.println("El nombre del empleado es "+this.nombre+" y su salario de: "+this.salario);
+	}
+	
+	public void impuestos() {
+		if(this.salario>1500000) {
+			System.out.println("El empleado "+this.nombre+" deberá pagar impuestos");
+		}else {
+			System.out.println("El empleado "+this.nombre+" no deberá pagar impuestos");
+		}
+	}
 
 }
