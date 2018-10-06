db5b36ee031d72364459f1e09db048aae9074bdc
cristianmarint <31068007+cristianmarint@users.noreply.github.com>
2018-10-06 17:41:41 -0500

    Update Semestre IV/Programacion Orientada a Objetos/2 Corte/proyecto/codigo/src/ordenamientos/mergesort.java

--- Semestre IV/Programacion Orientada a Objetos/2 Corte/proyecto/codigo/src/ordenamientos/mergesort.java
+++ Semestre IV/Programacion Orientada a Objetos/2 Corte/proyecto/codigo/src/ordenamientos/mergesort.java
@@ -13,12 +13,12 @@ public class mergesort {
 	
 	public static void merge(int vector[],int izq, int m, int der){
 		   int i, j, k;
-		   int [] vector2 = new int[vector.length]; //array auxiliar
+		   int [] vector2 = new int[vector.length]; //array auxiliar//
 		   for (i=izq; i<=der; i++) //copia ambas mitades en el array auxiliar
 		             vector2[i]=vector[i];
 
 		             i=izq; j=m+1; k=izq;
-		             while (i<=m && j<=der) //copia el siguiente elemento más grande
+		             while (i<=m && j<=der) //copia el siguiente elemento mï¿½s grande
 		             if (vector2[i]<=vector2[j])
 		                     vector[k++]=vector2[i++];
 		             else
