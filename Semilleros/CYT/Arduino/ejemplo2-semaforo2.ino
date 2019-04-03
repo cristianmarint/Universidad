d9137e4a180cd3523546d3613695bee6e35635fc
cristianmarint <31068007+cristianmarint@users.noreply.github.com>
2019-04-03 15:34:43 -0500

    Semaforo con tira led

--- Semilleros/CYT/Arduino/ejemplo2-semaforo2.ino
+++ Semilleros/CYT/Arduino/ejemplo2-semaforo2.ino
@@ -26,49 +26,69 @@ void setup()
  
 void loop()
 {
-  semaforo(0,1,2,1);
+  semaforo(0,10,2,15);
   estado=0;
 }
 
 
 // Usa la variable global del estado
 // Variables de tiempo para cada color del semaforo
-//            //turno   t. rojo   t. verde   t. amarillo
-void semaforo(int turno,float tr ,float te  ,float ta){
+//            turno     t. rojo   t. verde   t. amarillo 
+void semaforo(int turno,float tr ,float te  ,float ta){// 😉
     while(estado==turno){
-      // Serial.print("Estado=> ");Serial.println(estado);
-      // Serial.print("+Turno=>");Serial.println(turno);
+      Serial.print("Estado=> ");Serial.println(estado);
+      Serial.print("+Turno=>");Serial.println(turno);
+      int i = 0;
+      int c = 0;
 
-        // rojo
-        pixels.setPixelColor(0, colors[0][0], colors[0][1], colors[0][2] );
-        pixels.setPixelColor(1, colors[0][0], colors[0][1], colors[0][2] );
-        pixels.show();
-        delay(ut*tr);
-        // amarillo
-        pixels.setPixelColor(2, colors[1][0], colors[1][1], colors[1][2] );
-        pixels.setPixelColor(3, colors[1][0], colors[1][1], colors[1][2] );
-        pixels.show();
-        delay(ut*te);
-        // verde
-        pixels.setPixelColor(4, colors[2][0], colors[2][1], colors[2][2] );
-        pixels.setPixelColor(5, colors[2][0], colors[2][1], colors[2][2] );
-        pixels.show();
-        delay(ut*ta);
-
-      for( i = 0; i < pixels.numPixels(); i++)
-      {
-        pixels.setPixelColor(i,0);//OFF
-      }
+      // rojo
+      pixels.setPixelColor(0, colors[0][0], colors[0][1], colors[0][2] );
+      pixels.setPixelColor(1, colors[0][0], colors[0][1], colors[0][2] );
       pixels.show();
-        
+      delay(ut*tr);
 
-      estado++; 
+      // Intervalo rojo-amarillo
+      pixels.setPixelColor(1, colors[0][0], colors[0][1], colors[0][2] );
+      pixels.setPixelColor(2, colors[1][0], colors[1][1], colors[1][2] );
+      pixels.show();
+      pixels.setPixelColor(0,0);
+      pixels.setPixelColor(1,0);
+      delay(ut*(ta*0.05));
+
+      // amarillo
+      pixels.setPixelColor(2, colors[1][0], colors[1][1], colors[1][2] );
+      pixels.setPixelColor(3, colors[1][0], colors[1][1], colors[1][2] );
+      pixels.show();
+      delay(ut*te);
+
+      // Intervalo amarillo-verde
+      pixels.setPixelColor(3, colors[1][0], colors[1][1], colors[1][2] );
+      pixels.setPixelColor(4, colors[2][0], colors[2][1], colors[2][2] );
+      pixels.show();
+      pixels.setPixelColor(2,0);
+      pixels.setPixelColor(3,0);
+      delay(ut*(ta*0.02));
+
+      // verde
+      pixels.setPixelColor(4, colors[2][0], colors[2][1], colors[2][2] );
+      pixels.setPixelColor(5, colors[2][0], colors[2][1], colors[2][2] );
+      pixels.show();
+      delay(ut*ta);
+
+    for( i = 0; i < pixels.numPixels(); i++)
+    {
+      pixels.setPixelColor(i,0);//OFF
+    }
+    pixels.show();
+      
+
+    estado++; 
     }
 }
 
 
 // Circuito en:
-// https://www.tinkercad.com/things/0mWjvUwhhYG-ejemplo2-semaforo2/
+// https://www.tinkercad.com/things/0mWjvUwhhYG
 
 
 // tutorial para conectar la tira:
