e806c1e6359589a2bd38d5152032a50ad0ea2156
cristianmarint <31068007+cristianmarint@users.noreply.github.com>
2019-04-09 16:38:00 -0500

    Display con Arduino UNO
    Control de display atravez de Arduino UNO:
    
    - Caracteres especiales creados con bytes en una matriz en donde existen 5 columnas y 8 filas, Cada una de las filas inicia con la letra B acompañada por  números (0 ó 1)   que representan encendio o apagado de cada pixel.

---
+++ Semilleros/STEM/Arduino/display.ino
@@ -0,0 +1,115 @@
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+int i=0;
+
+void pantalla(int op,int col=0,int fil=0,int del=50);
+
+byte sonrisa[] = {
+  B00000,
+  B01010,
+  B01010,
+  B00000,
+  B10001,
+  B11011,
+  B01110,
+  B00000
+};
+byte corazon[] = {
+  B00000,
+  B01010,
+  B10101,
+  B10001,
+  B10001,
+  B01010,
+  B00100,
+  B00000
+};
+byte nConGorrito[] = {
+  B11111,
+  B00000,
+  B10111,
+  B11001,
+  B10001,
+  B10001,
+  B10001,
+  B00000
+};
+
+void setup() {
+  lcd.createChar(1, sonrisa);
+  lcd.createChar(2, corazon);
+  lcd.createChar(3, nConGorrito);
+  
+  lcd.begin(16, 2);
+      
+  lcd.setCursor(3, 0);
+  lcd.print("Feliz a");lcd.write(byte(3));lcd.print("o!");
+}
+void loop() {
+  while(i<2019){
+    lcd.setCursor(6, 1);
+    i == 2010 ? i+=9 : i+=10;
+    lcd.print( i );
+  }
+  delay(3000);
+  pantalla(2,0,1,200);
+  pantalla(1,0,1);
+  i=0;
+}
+
+
+/*
+ @Descripción realiza modificaciones al contenido de la pantalla
+ @op selecciona el modo de la funcion
+ @fil fila inicial para modificar el contenido
+ @col columna inicial para modificar el contenido
+ @del retrazo en el entre cada iteracion
+*/
+void pantalla(int op,int fil,int col,int del){
+  switch (op)
+  {
+    // limpia la pantalla lCD
+    case 1:
+          for(int fl = fil; fl < 16; fl++)
+          {
+            for(int cl = col; cl < 2; cl++)
+            {
+                    lcd.setCursor(fl, cl);
+                    lcd.print(" ");
+                    delay(del);
+            }
+          }
+    break;
+
+    // decoracion
+    case 2:
+          for(int fl = fil; fl < 16; fl++)
+          {
+            for(int cl = col; cl < 2; cl++)
+            {
+                    lcd.setCursor(fl, cl);
+                    lcd.write( byte( random(1,3) ) );
+                    delay(del);
+            }
+          }
+    break;
+  
+    default:
+    break;
+  }
+}
+
+
+
+// Circuito en:
+// https://www.tinkercad.com/things/34N3HBJ1lK6
+
+// Documentación:
+  // Circuito
+    // https://www.arduino.cc/en/Tutorial/HelloWorld  
+  // Caracteres especiales
+    // https://www.arduino.cc/en/Reference/LiquidCrystalCreateChar 
+    // https://maxpromer.github.io/LCD-Character-Creator/
